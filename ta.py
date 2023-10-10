06a72ba4942cae335a35054e0200a587b20ea7fd
Dark_Benky <matus.benky@gmail.com>
2023-10-10 22:00:31 +0200

    poc tpo graphs

--- ta.py
+++ ta.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import yfinance as yf
 import datetime as dt
+import plotly.subplots as sp
 
 st.set_page_config(
     layout="wide"
@@ -24,14 +25,14 @@ def get_data(ticket, period ,timeframe ):
         '1w' : 168
     }
     
-    for _ in range(30):
+    for _ in range(20):
         try:
             df = yf.download(ticket, start=temp - dt.timedelta(hours=converter[period]), end= temp, interval= timeframe)
             if df.empty == False:
                 data.append(df)
             temp = temp - dt.timedelta(hours=converter[period])
         except Exception as e:
-            st.write('Error', e)
+            #st.write('Error', e)
             break
     return data
 
@@ -41,12 +42,56 @@ def POC(data):
     for dt in data:
         temp = {}
         for close , volume in zip(dt['Close'], dt['Volume']):
-            if close not in temp:
+            if int(close) not in temp:
                 temp[int(close)] = int(volume)
             else:
                 temp[int(close)] += int(volume)
         POCS.append(temp)
-    return POCS         
+    return POCS
+
+@st.cache_data()
+def TPO(data):
+    TPOS = []
+    for dt in data:
+        temp = {}
+        for close in dt['Close']:
+            if int(close) not in temp:
+                temp[int(close)] = 1
+            else:
+                temp[int(close)] += 1
+        TPOS.append(temp)
+    return TPOS
+
+
+@st.cache_data()
+def create_bar_chart(data, name , time):
+    
+    fig = sp.make_subplots(rows=2, cols=len(data), subplot_titles=[f'' for i in range(len(data))])
+
+    for index, dt in enumerate(data):
+        dt = pd.DataFrame.from_dict(dt, orient='index').reset_index()
+        
+        if name == 'POC':
+            dt = dt.rename(columns={'index': 'Price', 0: 'POC'})
+        else:
+            dt = dt.rename(columns={'index': 'Price', 0: 'TPO'})
+        
+        # Add horizontal bar chart trace
+        if time == '1h':
+            time = 'hour'
+        elif time == '1d':
+            time = 'day'
+        elif time == '1w':
+            time = 'week'
+        
+        bar_trace = go.Histogram(x=dt[name], y=dt['Price'], orientation='h', name=f"{(19 - index)} {time} ago", marker=dict(color=px.colors.qualitative.Plotly[6]))
+        fig.add_trace(bar_trace, row=1, col=index+1)
+
+    # Update layout for subplot grid
+    fig.update_layout(title_text=f'{name} Chart')
+            
+    return fig
+
             
 timeframe = st.selectbox("timeframe", ["1h", "1d", "1w"])
 
@@ -55,21 +100,22 @@ ticket = "BTC-USD"
 if timeframe == "1h":
     interval = "1m"
     df = get_data(ticket, timeframe , interval)
-    # st.write(df)
     POCS = POC(df)
-    st.write(POCS)
+    TP0S = TPO(df)
+
 elif timeframe == "1d":
-    interval = "1h"
+    interval = "5m"
     df = get_data(ticket, timeframe , interval)
-    # st.write(df)
     POCS = POC(df)
-    st.write(POCS)
+    TP0S = TPO(df)
+
 elif timeframe == "1w":
     interval = "1h"
     df = get_data(ticket, timeframe , interval)
-    # st.write(df)
     POCS = POC(df)
-    st.write(POCS)
-    
+    TP0S = TPO(df)
 
     
+st.plotly_chart(create_bar_chart(POCS, 'POC' , timeframe), use_container_width=True)
+st.write('---')
+st.plotly_chart(create_bar_chart(TP0S, 'TPO' , timeframe), use_container_width=True)
